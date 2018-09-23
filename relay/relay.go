9f33753c2224c67ecb63f198d8c4fc183c8aff98
Anton A.M <mail@anton.am>
2018-09-23 20:18:55 +0000

    Add batched queries handler

--- relay/relay.go
+++ relay/relay.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"strings"
 
@@ -45,6 +46,61 @@ func UnmarshalSpec(id graphql.ID, v interface{}) error {
 
 type Handler struct {
 	Schema *graphql.Schema
+}
+
+type BatchedHandler struct {
+	Schema *graphql.Schema
+}
+
+type RequestData struct {
+	ID            string
+	Query         string                 `json:"query"`
+	Variables     map[string]interface{} `json:"variables"`
+	OperationName string                 `json:"operationName"`
+}
+
+type BatchedResponse struct {
+	ID string `json:"id"`
+	graphql.Response
+}
+
+func (h *BatchedHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	queryBytes, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	var responses []*BatchedResponse
+	var queries []*RequestData
+	err = json.Unmarshal(queryBytes, &queries)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	responsesChan := make(chan *BatchedResponse, len(queries))
+	for i := range queries {
+		qr := queries[i]
+		go func(query *RequestData, responsesChan chan *BatchedResponse) {
+			gqlResponse := h.Schema.Exec(r.Context(), query.Query, query.OperationName, query.Variables)
+			responsesChan <- &BatchedResponse{query.ID, *gqlResponse}
+		}(qr, responsesChan)
+	}
+
+	for i := 0; i < len(queries); i++ {
+		responses = append(responses, <-responsesChan)
+	}
+	close(responsesChan)
+
+	responseJSON, err := json.Marshal(responses)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(responseJSON)
 }
 
 func (h *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
